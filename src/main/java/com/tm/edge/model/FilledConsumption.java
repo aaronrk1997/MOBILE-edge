3930d603bef9e644d802faf9761facfdfe7c4e35
aaron <aaron.rombouts@gmail.com>
2022-11-16 16:29:08 +0100

    added todo

--- src/main/java/com/tm/edge/model/FilledConsumption.java
+++ src/main/java/com/tm/edge/model/FilledConsumption.java
@@ -1,6 +1,6 @@
 package com.tm.edge.model;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 import com.tm.edge.model.*;
@@ -12,6 +12,7 @@ public class FilledConsumption {
     private Integer count;
     private Integer score;
     private String remark;
+    private Date createdAt;
     private Beer beer;
 
     
@@ -22,6 +23,7 @@ public class FilledConsumption {
         setCount(consumption.getCount());
         setScore(consumption.getScore());
         setRemark(consumption.getRemark());
+        setCreatedAt(consumption.getCreatedAt());
         setBeer(beer);
         
     }
@@ -80,6 +82,14 @@ public class FilledConsumption {
 
     public void setBeer(Beer beer) {
         this.beer = beer;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 
 
