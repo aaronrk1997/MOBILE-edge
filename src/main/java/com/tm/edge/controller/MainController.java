3f4e4ade929e348368ce6621cb4deb7a54e6749a
aaron <aaron.rombouts@gmail.com>
2022-11-16 16:52:55 +0100

    added todo

--- src/main/java/com/tm/edge/controller/MainController.java
+++ src/main/java/com/tm/edge/controller/MainController.java
@@ -233,7 +233,7 @@ public class MainController {
 
     @PutMapping("/consumptions/{id}")
     public Consumption updateConsumption(@RequestBody Consumption consumption, @PathVariable String id) {
-        ResponseEntity<Consumption> response = restTemplate.exchange("http://"+ consumptionServiceBaseUrl + "/consumptions" + id, HttpMethod.PUT, new HttpEntity<>(consumption), new ParameterizedTypeReference<Consumption>() {
+        ResponseEntity<Consumption> response = restTemplate.exchange("http://"+ consumptionServiceBaseUrl + "/consumptions/" + id, HttpMethod.PUT, new HttpEntity<>(consumption), new ParameterizedTypeReference<Consumption>() {
         });
         return response.getBody();
     }
