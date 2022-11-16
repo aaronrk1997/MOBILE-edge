1324798204524370bd3cea9fe12fc7dae00612c2
aaron <aaron.rombouts@gmail.com>
2022-11-16 08:38:10 +0100

    added first rest templates

--- src/main/java/com/tm/edge/controller/MainController.java
+++ src/main/java/com/tm/edge/controller/MainController.java
@@ -19,14 +19,38 @@ public class MainController {
     @Autowired
     private RestTemplate restTemplate;
 
-/*     @Value("${beerService.baseurl}")
+    @Value("${beerService.baseurl}")
     private String beerServiceBaseUrl;
 
     @Value("${consumptionservice.baseurl}")
     private String consumptionServiceBaseUrl;
 
     @Value("${userservice.baseurl}")
-    private String userInfoServiceBaseUrl; */
+    private String userInfoServiceBaseUrl;
+
+    // returns the list of all available beers
+    @Getmapping("/beers")
+    public List<Beer> getBeers() {
+        ResponseEntity<List<Beer>> response = restTemplate.exchange(beerServiceBaseUrl + "/beers", HttpMethod.GET, null, new ParameterizedTypeReference<List<Beer>>() {
+        });
+        return response.getBody();
+    }
+
+    // returns the list of all breweries
+    @GetMapping("/breweries")
+    public List<Brewery> getBreweries() {
+        ResponseEntity<List<Brewery>> response = restTemplate.exchange(beerServiceBaseUrl + "/breweries", HttpMethod.GET, null, new ParameterizedTypeReference<List<Brewery>>() {
+        });
+        return response.getBody();
+    }
+
+    // returns a list of all users
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        ResponseEntity<List<User>> response = restTemplate.exchange(userInfoServiceBaseUrl + "/users", HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {
+        });
+        return response.getBody();
+    }
 
     
 }
